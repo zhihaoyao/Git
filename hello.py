aa442c1ce7d0026fcef1c65d8dc6c7c2ad553cc3
zyao <zyao@umc.edu>
2014-12-10 09:39:33 -0600

    change hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,28 @@
-#hello.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-print 'Hello World'
+'a hello world GUI example.'
+
+from Tkinter import *
+import tkMessageBox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        tkMessageBox.showinfo('Message', 'Hello, %s' % name)
+
+app = Application()
+app.master.title('Hello World')
+# message loop:
+app.mainloop()
